c18976b0c6058bb984e5308fc810fda0f5d7fdf5
grdino <67813644+grdino@users.noreply.github.com>
2025-08-03 10:34:01 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ export default function Home() {
     <Layout>
       <div style={{ textAlign: 'center' }}>
         <img src="/homepage-pic.png" alt="Hero" style={{ maxWidth: '100%' }} />
-        <p>Welcome to Isla Trapani, a beautiful neighborhood in AltaVela II.</p>
+        <h2>Welcome to Isla Trapani</h2>
+        <p>Discover the beauty and community of Isla Trapani in Altavela II, Nayarit.</p>
+        <p>Whether you are a resident or guest, we invite you to explore everything our community offers.</p>
         <Link href="/owners">Homeowner's Resources</Link>
       </div>
     </Layout>
