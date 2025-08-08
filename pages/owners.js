eceb6f8710f64b8329256bb714ef1c3b00da5a90
grdino <67813644+grdino@users.noreply.github.com>
2025-08-08 09:03:27 -0600

    Update owners.js

--- pages/owners.js
+++ pages/owners.js
@@ -75,7 +75,7 @@ export default function Owners() {
                </td>
                <td data-label="Documentos">
                  <a href="/owners-docs/combined-2025-07-05-esp.pdf">Español</a>
-                 <div className="no-hay-text">No Hay</div>
+                 <div className="no-hay-text">No Hay Acta Todavia</div>
                </td>
                <td data-label="Documents">
                  <a href="/owners-docs/no-english-available.pdf">English</a>
