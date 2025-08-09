a75d2ec6698ec998fc020c3ad6f24fdcdc04daf6
grdino <67813644+grdino@users.noreply.github.com>
2025-08-09 10:35:56 -0600

    Update owners.js

--- pages/owners.js
+++ pages/owners.js
@@ -36,7 +36,10 @@ export default function Owners() {
          <table className="responsive-table">
            <thead>
              <tr>
-               <th rowSpan="2">{t("tt_date")}</th>
+               <th rowSpan="2">
+                <strong>{t("tt_date")}</strong>{" "}
+                <span className="date-format">{t("tt_date_fmt")}</span>
+               </th>
                <th rowSpan="2">Tipo de Documento<br />Document Type</th>
                <th colSpan="2">Documento<br />Document</th>
              </tr>
