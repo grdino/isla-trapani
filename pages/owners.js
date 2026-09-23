f6abe2afabbddde550df9f7cb5aaac75de624531
grdino <67813644+grdino@users.noreply.github.com>
2026-09-23 16:52:18 -0600

    Update owners.js
    duh

--- pages/owners.js
+++ pages/owners.js
@@ -171,7 +171,7 @@ export default function Owners() {
                   {t('td_agm')}
                 </td>
                 <td data-label={t('tt_links')}>
-                  <a href="/owners-docs/combined-2026-08-15-esp.pdf.pdf">Español</a>
+                  <a href="/owners-docs/combined-2026-08-15-esp.pdf">Español</a>
                 </td>
                 <td data-label={t('tt_links')}>
                   <a href="/owners-docs/combined-2026-08-15-eng.pdf">English</a>
